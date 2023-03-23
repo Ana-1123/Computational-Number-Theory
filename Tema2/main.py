befa7aa5dac0f5184e13af8e3d9c50a76971352c
Ciobanu Ana <75687295+Ana-1123@users.noreply.github.com>
2023-03-24 01:43:13 +0200

    Add files via upload

--- Tema2/main.py
+++ Tema2/main.py
@@ -3,6 +3,10 @@ from Cryptodome.Util.number import getPrime, inverse
 import math
 import time
 
+list_lr_bin = []
+list_lr_fixed_wind = []
+list_lr_slid_wind = []
+
 
 def generare_param():
     p = getPrime(700, randfunc=get_random_bytes)
@@ -82,10 +86,14 @@ def lr_bin_exp(x, n, m):
     n_b = list(map(int, bin(n).replace("0b", "")))
     k = len(n_b)
     y = 1
+    lungimea = 0
     for i in range(0, k):
         y = (y * y) % m
         if n_b[i] == 1:
             y = (y * x) % m
+            lungimea += 1
+    print("Lungimea pentru left right binary: ", lungimea)
+    list_lr_bin.append(lungimea)
     return y
 
 
@@ -106,14 +114,19 @@ def lr_fixed_wind(x, n, m, base):
     for i in range(0, base):
         x_list.append(0)
     x_list[0] = 1
+    lungime = 0
     for i in range(1, base):
         x_list[i] = (x_list[i - 1] * x) % m
+        lungime += 1
     n = list(map(int, bin(n).replace("0b", "")))
     k = len(n)
     y = 1
     for i in range(k):
         y = pow(y, base, m)
         y = y * x_list[n[i]] % m
+        lungime += 1
+    print("Lungimea pentru left right fixed window: ", lungime)
+    list_lr_fixed_wind.append(lungime)
     return y
 
 
@@ -129,37 +142,37 @@ def multi_prime_rsa_lr_fixed_wind(p, q, r, e, d, y):
     print("Timpul decriptarii utilizand TCR cu algoritmul Garner lr_fixed_wind:", fc_time)
 
 
-def lr_slid_wind(x, n, m, w=3):
+def lr_slid_wind(x, n, m, w=4):
     xi = dict()
     xi[1] = x % m
-    xi[2] = (xi[1] * xi[1]) % m
-    for omega in range(3, pow(2, w), 2):
-        xi[omega] = (xi[omega - 2] * xi[2]) % m
+    xi[2] = (xi.get(1) * xi.get(1)) % m
+    lungimea = 1
+    for omega in range(3, 2 ** w, 2):
+        xi[omega] = xi.get(omega - 2) * xi.get(2) % m
+        lungimea += 1
     n_b = list(map(int, bin(n).replace("0b", "")))
+    n_b = list(reversed(n_b))
     y = 1
     i = len(n_b) - 1
     while i >= 0:
         if n_b[i] == 0:
-            y = (y * y) % m
+            y = y * y % m
             i -= 1
-        elif i == 0 and n_b[i] == 1:
-            y = (y * y) % m
-            y = (y * xi[1]) % m
-            i = i - 1
         else:
+            nr = 0
             j = max(i - w + 1, 0)
             while j < i and n_b[j] != 1:
                 j += 1
-            for l in range(i - j + 1):
-                y = (y * y) % m
-            if j == 0:
-                nr_list = ''.join(map(str, n_b[i::-1]))
-            else:
-                nr_list = ''.join(map(str, n_b[i:j - 1:-1]))
-            ni_nj = int(nr_list, 2)
-            y = (y * xi[ni_nj]) % m
+            for lnr in range(1, i - j + 2):
+                y = y * y % m
+            for k in range(i, j - 1, -1):
+                nr = nr * 10 + n_b[k]
+            ni_nj = int(str(nr), 2)
+            y = y * xi.get(ni_nj) % m
+            lungimea += 1
             i = j - 1
-
+    print("Lungimea pentru left-right sliding window: ", lungimea)
+    list_lr_slid_wind.append(lungimea)
     return y
 
 
@@ -198,14 +211,23 @@ if __name__ == '__main__':
 
     multi_prime_rsa_lr_slid_wind(p, q, r, e, d, y)
 
+    average_l_bin = sum(list_lr_bin) / len(list_lr_bin)
+    average_l_fixed = sum(list_lr_fixed_wind) / len(list_lr_fixed_wind)
+    average_l_slid = sum(list_lr_slid_wind) / len(list_lr_slid_wind)
+    average_list = [average_l_bin, average_l_fixed, average_l_slid]
+    denumiri_metode = ["Left-right binary method", "Left-right fixed window method", "Left-right sliding window method"]
+    max_l = max(average_list)
+    min_l = min(average_list)
+    print("Lungime maxima are: ", denumiri_metode[average_list.index(max_l)])
+    print("Lungime minima are: ", denumiri_metode[average_list.index(min_l)])
     n = p * p * q
     phi = (p * p - p) * (q - 1)
     while math.gcd(e, phi) != 1:
         e = getPrime(16, randfunc=get_random_bytes)
     d = inverse(e, phi)
     x = input("Introduceti un numar:")
-    y = pow(int(x), e, n)
-    print("Numarul criptat: ", y)
-    # y = int(x)
+    # y = pow(int(x), e, n)
+    # print("Numarul criptat: ", y)
+    y = int(x)
     multi_power_rsa(p, q, r, e, d, y)
     decriptare_librarii(y, n)
